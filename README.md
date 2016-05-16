c016a6d577b2ace86bb7b8eb8a32c2376c062b7f
nel215 <otomo.yuhei@gmail.com>
2016-05-16 23:48:15 +0900

    added benchmark test

--- README.md
+++ README.md
@@ -11,6 +11,12 @@ see [here](./example_test.go)
 # Performance
 
 ```
-BenchmarkStlmapConcurrent-4     20000000               260 ns/op
-BenchmarkDefaultMapConcurrent-4 20000000               310 ns/op
+BenchmarkStlmapConcurrent-4             20000000               257 ns/op
+BenchmarkDefaultMapConcurrent-4         20000000               315 ns/op
+BenchmarkStlmapConcurrentSet-4          20000000               211 ns/op
+BenchmarkDefaultMapConcurrentSet-4      20000000               223 ns/op
+BenchmarkStlmapConcurrentGet-4          30000000               142 ns/op
+BenchmarkDefaultMapConcurrentGet-4      20000000               161 ns/op
+BenchmarkStlmapConcurrentDelete-4       30000000               154 ns/op
+BenchmarkDefaultMapConcurrentDelete-4   30000000               167 ns/op
 ```
--- stlmap_test.go
+++ stlmap_test.go
@@ -105,3 +105,114 @@ func BenchmarkDefaultMapConcurrent(b *testing.B) {
 		}
 	})
 }
+
+func BenchmarkStlmapConcurrentSet(b *testing.B) {
+	do := func(m *StripedLockedMap, keys []string) {
+		key := keys[rand.Int()%len(keys)]
+		m.Set(key, 1)
+	}
+	m := New(&Config{})
+	keys := getKeys(1000)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			do(m, keys)
+		}
+	})
+}
+
+func BenchmarkDefaultMapConcurrentSet(b *testing.B) {
+	do := func(mu *sync.RWMutex, mp map[string]interface{}, keys []string) {
+		key := keys[rand.Int()%len(keys)]
+		mu.Lock()
+		mp[key] = 1
+		mu.Unlock()
+	}
+	mu := &sync.RWMutex{}
+	mp := make(map[string]interface{})
+	keys := getKeys(1000)
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			do(mu, mp, keys)
+		}
+	})
+}
+
+func BenchmarkStlmapConcurrentGet(b *testing.B) {
+	do := func(mp *StripedLockedMap, keys []string) interface{} {
+		key := keys[rand.Int()%len(keys)]
+		return mp.Get(key)
+	}
+	mp := New(&Config{})
+	keys := getKeys(1000)
+	for _, k := range keys {
+		mp.Set(k, 1)
+	}
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			do(mp, keys)
+		}
+	})
+}
+
+func BenchmarkDefaultMapConcurrentGet(b *testing.B) {
+	do := func(mu *sync.RWMutex, mp map[string]interface{}, keys []string) interface{} {
+		key := keys[rand.Int()%len(keys)]
+		mu.RLock()
+		res := mp[key]
+		mu.RUnlock()
+		return res
+	}
+	mu := &sync.RWMutex{}
+	mp := make(map[string]interface{})
+	keys := getKeys(1000)
+	for _, k := range keys {
+		mp[k] = 1
+	}
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			do(mu, mp, keys)
+		}
+	})
+}
+
+func BenchmarkStlmapConcurrentDelete(b *testing.B) {
+	do := func(mp *StripedLockedMap, keys []string) {
+		key := keys[rand.Int()%len(keys)]
+		mp.Delete(key)
+	}
+	mp := New(&Config{})
+	keys := getKeys(1000)
+	for _, k := range keys {
+		mp.Set(k, 1)
+	}
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			do(mp, keys)
+		}
+	})
+}
+
+func BenchmarkDefaultMapConcurrentDelete(b *testing.B) {
+	do := func(mu *sync.RWMutex, mp map[string]interface{}, keys []string) {
+		key := keys[rand.Int()%len(keys)]
+		mu.Lock()
+		delete(mp, key)
+		mu.Unlock()
+	}
+	mu := &sync.RWMutex{}
+	mp := make(map[string]interface{})
+	keys := getKeys(1000)
+	for _, k := range keys {
+		mp[k] = 1
+	}
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			do(mu, mp, keys)
+		}
+	})
+}
